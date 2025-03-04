15ab411ad013379e22d5a853597618e1bdfdf712
mumenr-glitch <ivanruiz343@gmail.com>
2025-03-04 14:16:14 -0800

    [any message]

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <zlib.h>
-
+#include <openssl/sha.h>
 
 
 int main(int argc, char *argv[]) {
@@ -45,7 +45,7 @@ int main(int argc, char *argv[]) {
 
     else if (strcmp(command, "cat-file") == 0){
         if (argc < 4 || strcmp(argv[2], "-p") != 0){
-            fprintf(stderr, "Usage: ./your_program cat-file -p <object_hash>\n", strerror(errno));
+            fprintf(stderr, "Usage: ./your_program cat-file -p <object_hash>\n");
             return 1;
         }
         
@@ -155,6 +155,139 @@ int main(int argc, char *argv[]) {
         printf("%s", content);
 
         free(decompressed_data);
+    }
+
+    else if (strcmp(command, "hash-object") == 0){
+        if (argc < 4 || strcmp(argv[2], "-w") != 0){
+            fprintf(stderr, "Usage: ./your-program hash-object -w <file>\n");
+            return 1;
+        }
+        const char *file_name = argv[3];
+
+        // Open file and validate
+        FILE *file = fopen(file_name, "rb");
+        if (!file){
+            fprintf(stderr, "Error opening file %s: %s\n", file_name, strerror(errno));
+            return 1;
+        }
+
+        // Get the size of the file
+        fseek(file, 0, SEEK_END);
+        long file_size = ftell(file);
+        rewind(file);
+
+        // Allocate appropiate amount of memory with validation
+        unsigned char *file_content = malloc(file_size);
+        if (!file_content){
+            fprintf(stderr, "Memory allocation failed");
+            fclose(file);
+            return 1;
+        }
+
+        // Read the file then close it once file_content has the data
+        fread(file_content, 1, file_size, file);
+        fclose(file);
+
+        // Build the blob
+        char header[64];
+        int header_length = snprintf(header, sizeof(header), "blob %ld", file_size);
+
+        // Allocate appropriate memory for blob with validation
+        unsigned long blob_size = header_length + 1 +file_size;
+        unsigned char *blob = malloc(blob_size);
+        if (!blob){
+            fprintf(stderr, "Memory allocation failed for blob\n");
+            free(file_content);
+            return 1;
+        }
+
+        // Add to blob the header first
+        memcpy(blob, header, header_length);
+
+        // Add the null after the header
+        blob[header_length] = '\0';
+
+        // Add the file contents to the blob
+        memcpy(blob + header_length + 1, file_content, file_size);
+
+        // SSH build
+        unsigned char sha1_hash[SHA_DIGEST_LENGTH]; // 20 bytes in length
+        SHA1(blob, blob_size, sha1_hash);
+
+        // Convert binary hash to hex hash
+        char sha1_hash_hex[41]; // 40 chars but 1 byte for null
+        for (int i = 0; i < SHA_DIGEST_LENGTH; i++){
+            sprintf(sha1_hash_hex + (i * 2), "%02x", sha1_hash[i]);
+        }
+        sha1_hash_hex[40] = '\0';
+        
+        // Create directory and filename
+        char dir[3];
+        char filename[39];
+        strncpy(dir, sha1_hash_hex, 2);
+        dir[2] = '\0';
+        strncpy(filename, sha1_hash_hex + 2, 38);
+        filename[38] = '\0';
+        
+        // Create the actual directory
+        char dir_path[256];
+        snprintf(dir_path, sizeof(dir_path), ".git/objects/%s", dir);
+
+        if (mkdir(dir_path, 0755) == -1 && errno!= EEXIST){
+            fprintf(stderr, "Failed to create directory %s: %s\n", dir_path, strerror(errno));
+            free(blob);
+            free(file_content);
+            return 1;
+        }
+
+        char object_path[256];
+        snprintf(object_path, sizeof(object_path), ".git/objects/%s/%s", dir, filename);
+
+        //Alocate space for data
+        unsigned long compressed_max_size = compressBound(blob_size);
+        unsigned char *compressed_blob = malloc(compressed_max_size);
+        if (!compressed_blob){
+            fprintf(stderr, "Memory allocation failed for compressed blob\n");
+            free(blob);
+            free(file_content);
+            return 1;
+        }
+
+        // Perform compression
+        if (compress(compressed_blob, &compressed_max_size, blob, blob_size) != Z_OK){
+            fprintf(stderr, "Compression failed\n");
+            free(blob);
+            free(file_content);
+            free(compressed_blob);
+            return 1;
+        }
+
+        // Open the objext file for writing
+        FILE *object_file = fopen(object_path, "wb");
+        if (!object_file){
+            fprintf(stderr, "Failed to create object file %s: %s\n", object_path, strerror(errno));
+            free(blob);
+            free(file_content);
+            free(compressed_blob);
+            return 1;
+        }
+
+        // Write the compressed blob into the file
+        if (fwrite(compressed_blob, 1, compressed_max_size, object_file) != compressed_max_size){
+            fprintf(stderr, "Failed to write compressed data to file\n");
+            fclose(object_file);
+            free(blob);
+            free(file_content);
+            free(compressed_blob);
+            return 1;
+        }
+
+        // Clean up
+        fclose(object_file);
+        free(blob);
+        free(file_content);
+        free(compressed_blob);
+
     }
     else {
         fprintf(stderr, "Unknown command %s\n", command);
